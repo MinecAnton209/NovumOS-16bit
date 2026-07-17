65f26b0a23e6eb52144b53543b51bd744976e6a4
MinecAnton209 <poterbaanton90@gmail.com>
2026-07-17 11:39:01 +0300

    refactor: remove codename, rename to NovumOS 16bit
    Drop 'Iron Gate' codename and rename from 'NovumOS-16bit' to
    'NovumOS 16bit' for the boot message.

--- src/versioning.zig
+++ src/versioning.zig
@@ -3,7 +3,6 @@
 /// Central version information for the OS.
 
 pub const NOVUMOS_VERSION = "0.1.0";
-pub const NOVUMOS_CODENAME = "Iron Gate";
 
-pub const NOVUMOS_NAME = "NovumOS-16bit";
-pub const NOVUMOS_FULL = NOVUMOS_NAME ++ " v" ++ NOVUMOS_VERSION ++ " (" ++ NOVUMOS_CODENAME ++ ")";
+pub const NOVUMOS_NAME = "NovumOS 16bit";
+pub const NOVUMOS_FULL = NOVUMOS_NAME ++ " (v" ++ NOVUMOS_VERSION ++ ")";
